6cae75ab33d9ad848bbde113ce10e88e71d50301
falcoca <falcoca@gmail.com>
2020-03-06 20:24:25 +0000

    friday progress

--- app.rb
+++ app.rb
@@ -14,5 +14,91 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+locations_table = DB.from(:locations)
+rikis_table = DB.from(:rikis)
+areas_table = DB.from(:areas)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    puts "params: #{params}"
+    @areas = areas_table.all.to_a
+    view "home"
+end
+
+get "/locations" do
+    puts "params: #{params}"
+
+    @locations = locations_table.all.to_a
+    view "locations"
+end
+
+get "/locations/:id" do
+    puts "params: #{params}"
+    @locations = locations_table.all.to_a
+    @location = locations_table.where(id: params[:id]).to_a[0]
+    view "location"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
+
+get "/areas/:id" do
+    @area = areas_table.where(id: params[:id]).to_a[0]
+    @locations = locations_table.where(areas_id: @area[:id])
+    view "area"
+end
+
+post "/rikis/submit" do
+    puts params
+    
+    rikis_table.insert(purpose: params["purpose"], 
+        rating: params["rating"], 
+        comments: params["comments"])
+    
+    view "submit_riki"
+end
+
+post "/locations/submit" do
+    puts params
+    
+    locations_table.insert(name: params["name"], 
+        description: params["description"], 
+        address: params["address"],
+        areas_id: params["areas_id"])
+    
+    view "submit_area"
+end
--- createdb.rb
+++ createdb.rb
@@ -4,32 +4,116 @@ connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqli
 DB = Sequel.connect(connection_string)                                                #
 #######################################################################################
 
-# Database schema - this should reflect your domain model
-DB.create_table! :events do
+# Database Schema - Reflects Domain Model
+#Locations Table
+DB.create_table! :locations do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
+  foreign_key :areas_id
+  String :name
+  String :description
+  String :address
 end
-DB.create_table! :rsvps do
+#Riki's Table
+DB.create_table! :rikis do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :locations_id
+  foreign_key :users_id
+  Integer :rating
+  String :purpose
+  String :comments
+end
+#Areas Table
+DB.create_table! :areas do
+  primary_key :id
+  String :name
+end
+#Users Table
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
-
-# Insert initial (seed) data
-events_table = DB.from(:events)
-
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
-
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+# Areas Initial (Seed) Data
+areas_table = DB.from(:areas)
+areas_table.insert(name: "Evanston")
+areas_table.insert(name: "Lincoln Park")
+areas_table.insert(name: "Old Town")
+areas_table.insert(name: "River North")
+areas_table.insert(name: "West Loop")
+areas_table.insert(name: "Wicker Park")
+#Locations Initial (Seed) - Evanston
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Whiskey Thief", 
+                    description: "Dark and Sinister",
+                    address: "616 Davis St, Evanston, IL 60201",
+                    areas_id: 1)
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Smylie Brothers", 
+                    description: "Great BBQ & Brew",
+                    address: "1615 Oak Ave, Evanston, IL 60201",
+                    areas_id: 1)
+#Locations Initial (Seed) - Lincoln Park
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Delilah's Chicago", 
+                    description: "300 Varieties of Whiskey",
+                    address: "2771 N Lincoln Ave, Chicago, IL 60614",
+                    areas_id: 2)
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Alinea", 
+                    description: "Save It For The One...",
+                    address: "1723 N Halsted St, Chicago, IL 60614",
+                    areas_id: 2)    
+#Locations Initial (Seed) - Old Town
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The VIG Chicago", 
+                    description: "Warehouse + Gastropub Feel",
+                    address: "1527 N Wells St, Chicago, IL 60610",
+                    areas_id: 3)
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Old Town Ale House", 
+                    description: "Cash-Only",
+                    address: "219 W North Ave, Chicago, IL 60610",
+                    areas_id: 3)    
+#Locations Initial (Seed) - River North
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Untitled", 
+                    description: "Huge Bar! A Kellogg Staple...",
+                    address: "111 W Kinzie St, Chicago, IL 60654",
+                    areas_id: 4)
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Bub City", 
+                    description: "Live Country Music!",
+                    address: "435 N Clark St, Chicago, IL 60654",
+                    areas_id: 4)    
+#Locations Initial (Seed) - West Loop
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Moneygun", 
+                    description: "Loud Rap Music!",
+                    address: "660 W Lake St",
+                    areas_id: 5)
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The Aviary", 
+                    description: "Over-the-Top Cocktails",
+                    address: "955 W Fulton Market, Chicago, IL 60607",
+                    areas_id: 5)  
+#Locations Initial (Seed) - Wicker Park
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The Violet Hour", 
+                    description: "Chic Speakeasy",
+                    address: "1520 N Damen Ave, Chicago, IL 60622",
+                    areas_id: 6)
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The Revel Room", 
+                    description: "Vintage Furnishings and Walls of Books",
+                    address: "1566 N Milwaukee Ave, Chicago, IL 60622",
+                    areas_id: 6)    
+#Users (Seed)
+users_table = DB.from(:users)
+users_table.insert(name: "Riki",
+                email: "rikinder.mahal@gmail.com", 
+                password: "Dog")
+users_table.insert(name: "Christian",
+                email: "falcoca@gmail.com", 
+                password: "Riki")
+#Riki's (Seed)
